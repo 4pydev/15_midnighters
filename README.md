13b4b2e79ec9752fb39078ddb0b0a5aea96ba364
4pydev <4pydev@gmail.com>
2017-11-26 17:25:33 +0300

    Added seek_dev_nighters.py and README.md

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Скрипт находит на [devman](https://devman.org) "сов", т.е. людей, отправивших свои задания на проверку между 12:00 ночи и 05:00 утра.
 
+# Исползование и пример вывода
+
+Для использования необходим установленный Python 3.5, а также модуль pytz.
+
+```bush
+$ sudo pip3 install pytz # установка pytz
+
+$ python3 seek_dev_nighters.py # запуск скрипта
+
+1  :  vladmalakhov
+2  :  oleg.ivliev1
+3  :  evgeniy.rossoshanskiy
+...
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,40 @@
+#!/usr/bin/env python3
+
+from datetime import datetime, time
+import requests
+import pytz
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    pages = 10
+    for page in range(1, pages+1):
+        url = 'https://devman.org/api/challenges/solution_attempts/'
+        params = {
+            'page': page
+            }
+        users_attempts = requests.get(url, params).json()['records']
+        for attempt in users_attempts:
+            yield attempt
+
+
+def is_midnighter(user_attempt):
+    user_time_zone = user_attempt['timezone']
+    user_utc_datetime = datetime.fromtimestamp(user_attempt['timestamp'])
+    user_local_time = pytz.timezone(
+        user_time_zone).fromutc(user_utc_datetime).time()
+    return True if time(5, 0, 0) > user_local_time > time(0, 0, 0) else False
+
 
 def get_midnighters():
-    pass
+    owl_users = set()
+    for attempt in load_attempts():
+        if is_midnighter(attempt):
+            owl_users.add(attempt['username'])
+    return owl_users
+
 
 if __name__ == '__main__':
-  pass
+    i = 1
+    for user in get_midnighters():
+        print(i, ' : ', user)
+        i += 1
